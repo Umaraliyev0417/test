f4744ee7fc157ad5faa4043aef19529e0873bb94
Umaraliyev0417 <umaraliyev0417@gmail.com>
2023-11-24 18:15:17 +0500

    Work

---
+++ main.py
@@ -0,0 +1,73 @@
+# list = [1, 3 , 4, "Damas", "Captiva", True]
+
+
+# Цикл for
+# for x in list: #list bo'yicha tekshiruv olib boradi.
+#     print(x)
+#     if x == "Damas":
+#         print(f"{x} mashinasi bu ro'yxatda bor!")
+#         break # x topilganda qidiruv to'xtasin
+#     else:
+#         print("Bu ro'yxatda yo'q!")
+
+# for x in list: #list bo'yicha tekshiruv olib boradi.
+#     if x == "Damas":
+#         continue # x topilganda qidiruv to'xtasin
+#
+#     elif x == "Captiva":
+#         print("Bor")
+#         break
+#
+#     else:
+#         print("Bu ro'yxatda yo'q!")
+
+# 1 an n gacha sonlarni yig'indisi
+# while True:
+#   number = int(input('Sonni kiriting: '))
+#
+#   summa = 0
+#   num = range(1, number + 1)
+#   for x in num:
+#       summa = summa + x
+#   print(summa)
+
+# while True:
+#     numb = int(input("Enter any integer: "))
+#     num = range(1,11)
+#     for x in num:
+#         if numb % x == 0:
+#             print(f"{numb} soni {x} ga qoldiqsiz bo'linadi")
+#
+#         else:
+#             print("error")
+
+
+# list = [[2,6,8], [1,3]]
+#
+# for x in list: #listni ichidan yana list kesa yana 1 ta for bn tekshiramiz
+#     for y in x:
+#         if y == 6:
+#             print('Bor')
+
+
+
+
+
+# num = int(input("sonni kiriting: "))
+#
+# if num < 0:
+#     print("manfiy")
+# elif num > 0:
+#     print("musbat")
+
+
+
+
+
+
+
+
+
+
+
+
